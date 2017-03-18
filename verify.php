5c983c49fb9cfbc70ccff265632a588021118c5a
psorkhay <psorkhay@gmail.com>
2017-03-18 14:23:33 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'U1eba6691b4f382aa1e8f23e27b26d3a9';
+$access_token = 'UQ/cCzgXdQlAB+/w4W4pBWuGRaVddGrs/UgC4U05kaaq9M5OVKZE2HcB6T/73pQ8iBjtr864tuq3wKca7XeYkGqae4okCad1W4Z1fYp57uDwcRlrFLEBSiYKtRI19vB30iYKk6waarKo+tnpIkek5QdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
